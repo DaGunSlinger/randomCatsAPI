f41c945187a2f1a9b27b7ceb86a4a7f42e0d19ad
Sebastian Hernandez <dagunslinger.developer@gmail.com>
2023-02-10 20:24:26 -0500

    Se generan funciones para favorite y se arregla visualizacion

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-let API_URL = 'https://api.thecatapi.com/v1/images/search';
-const query_param = '?limit=5'
+//API URL
+let API_URL = 'https://api.thecatapi.com/v1/'; //v1
+let API_URL_ERR = 'https://http.cat/';
+//endpoitns
+const methodGET = 'images/search'
+const queryLimit = '?limit=5'
+const favorites = 'favourites'
+//key
 const API_KEY = '&api_key=live_baeVDLnjkGPGw4iWNwc5CEE8wYwetNLHlaiHJw7uYLu2Cvb4UECgTLIksB9jUgJO'
-API_URL = API_URL + query_param + API_KEY;
+//constructor
+API_URL_GENERAL = API_URL + methodGET + queryLimit + API_KEY;
+API_URL_FAVS = API_URL + favorites + queryLimit + API_KEY;
 
 const img1 = document.querySelector('.kitty-1');
 const img2 = document.querySelector('.kitty-2');
@@ -18,20 +26,45 @@ const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10]
 
 
 const refreshBtn = document.querySelector('.refresh');
+const spanErr = document.querySelector('.status');
 
 
 refreshBtn.addEventListener('click', refreshCats)
 
 async function refreshCats(){
-    const res = await fetch(API_URL);
-    console.log(API_URL);
-    let dataJson = await res.json();
-    for (let i = 0; i < images.length; i++) {
-        images[i].src = dataJson[i].url
+    try{
+        const res = await fetch(API_URL_GENERAL);
+        let dataJson = await res.json();
+        for (let i = 0; i < 5; i++) {
+            images[i].src = dataJson[i].url
+        }
+    } catch(err){
+        console.log(catError);
+    }
+}
+
+async function loadFavoriteCats(){
+    try{
+        const res = await fetch(API_URL_FAVS);
+        const dataJson = await res.json();
+        console.log(dataJson);
+    } catch(err){
+        console.log("el error es: " + err.message);
+    }
+}
+
+async function saveCats(){
+    try{
+        const res = await fetch(API_URL_FAVS);
+        const dataJson = await res.json();
+        console.log(dataJson);
+    } catch(err){
+        console.log("el error es: " + err.message);
     }
 }
 
 refreshCats()
+loadFavoriteCats()
 
 /* Lógica básica
 fetch(URL)                      //traigo con fetch
